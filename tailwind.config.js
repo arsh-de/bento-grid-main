4b168192703123ddf83a107cf6923de369cd95f3
arsh-de <singharsh0811@gmail.com>
2024-11-29 16:52:32 -0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                Purple100: 'hsl(254, 88%, 90%)',
+                Purple500: 'hsl(256, 67%, 59%)',
+                Yellow100: 'hsl(31, 66%, 93%)',
+                Yellow500: 'hsl(39, 100%, 71%)',
+                White: 'hsl(0, 0%, 100%)',
+                WhiteBg: 'hsl(0, 20%, 96%)',
+                Black: 'hsl(0, 0%, 7%)',
+            },
+        },
+        fontFamily: {
+            DMSans: ['DM Sans', 'sans-serif'],
+        },
+    },
+    plugins: [],
+}
